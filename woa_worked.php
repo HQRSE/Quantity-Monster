e5547a688115b0fce0d0d7c725c6721c05c192cc
HQRSE <60251201+HQRSE@users.noreply.github.com>
2020-03-17 12:46:21 +0300

    Update woa_worked.php

--- woa_worked.php
+++ woa_worked.php
@@ -61,43 +61,43 @@ echo "all items: ".$all_items."<br>";
 
 while ($h_counter < $end) {
 
-$code = $assoc[0]['val'][$h_counter]['atr']['GUID'];
-$results = $DB->Query("SELECT IBLOCK_ELEMENT_ID FROM b_iblock_element_property WHERE VALUE='$code' AND DESCRIPTION='Код'");
-	while ($row = $results->Fetch())
+	$code = $assoc[0]['val'][$h_counter]['atr']['GUID'];
+	$results = $DB->Query("SELECT IBLOCK_ELEMENT_ID FROM b_iblock_element_property WHERE VALUE='$code' AND DESCRIPTION='Код'");
+		while ($row = $results->Fetch())
 		{
 		$res = CIBlockElement::GetList(array(), array('IBLOCK_ID' => 10, 'ID' => $row['IBLOCK_ELEMENT_ID'], 'SITE_ID' => "s1"));
         $item = $res->Fetch();
-		if (!empty($item['ID']) && $item['ACTIVE'] = 'Y') { 
-		$el = $item['ID']; 
+			if (!empty($item['ID']) && $item['ACTIVE'] = 'Y') { 
+			$el = $item['ID']; 
+			}
 		}
-	}
 
-$file_c = 0;
+	$file_c = 0;
 
-while (count($assoc[0]['val'][$h_counter]['val'][0]['val']) > $file_c) {
+	while (count($assoc[0]['val'][$h_counter]['val'][0]['val']) > $file_c) {
 
-	$sh_guid = $assoc[0]['val'][$h_counter]['val'][0]['val'][$file_c]['atr']['GUID'];
-	$sh_qty = $assoc[0]['val'][$h_counter]['val'][0]['val'][$file_c]['atr']['Quantity'];
-	$results_sh_id = $DB->Query("SELECT ID FROM b_catalog_store WHERE XML_ID='$sh_guid'");
+		$sh_guid = $assoc[0]['val'][$h_counter]['val'][0]['val'][$file_c]['atr']['GUID'];
+		$sh_qty = $assoc[0]['val'][$h_counter]['val'][0]['val'][$file_c]['atr']['Quantity'];
+		$results_sh_id = $DB->Query("SELECT ID FROM b_catalog_store WHERE XML_ID='$sh_guid'");
 
-	if ($row_sh_id = $results_sh_id->Fetch()) {
-		$shop_id = $row_sh_id['ID'];
+			if ($row_sh_id = $results_sh_id->Fetch()) {
+			$shop_id = $row_sh_id['ID'];
+			}
 
-	}
-
-	$results_qty = $DB->Query("SELECT * FROM b_catalog_store_product WHERE STORE_ID='$shop_id' AND PRODUCT_ID='$el'"); //$el //$code
-	if ($row_qty = $results_qty->Fetch()) {
-		if ($sh_qty !== $row_qty['qty']) {
-			$results_upd = $DB->Query("UPDATE b_catalog_store_product SET AMOUNT = '$sh_qty' WHERE STORE_ID = '$shop_id' AND PRODUCT_ID = '$el'");
-		}
-	} else {
-		$results_insert = $DB->Query("INSERT INTO b_catalog_store_product (PRODUCT_ID, AMOUNT, STORE_ID) VALUES ('$el', '$sh_qty', '$shop_id')");
-	}
+		$results_qty = $DB->Query("SELECT * FROM b_catalog_store_product WHERE STORE_ID='$shop_id' AND PRODUCT_ID='$el'"); //$el //$code
+			if ($row_qty = $results_qty->Fetch()) {
+				if ($sh_qty !== $row_qty['qty']) {
+				$results_upd = $DB->Query("UPDATE b_catalog_store_product SET AMOUNT = '$sh_qty' WHERE STORE_ID = '$shop_id' AND PRODUCT_ID = '$el'");
+				}
+			} else {
+			$results_insert = $DB->Query("INSERT INTO b_catalog_store_product (PRODUCT_ID, AMOUNT, STORE_ID) VALUES ('$el', '$sh_qty', '$shop_id')");
+			}
 		$file_c++;
-}
+	}
 
 	$load_time = mktime() - $load_time_start;
 	$all_time = $all_time + $load_time;
+	
 	$z = $h_counter % $step;
 
 	if ($z == 0) {
